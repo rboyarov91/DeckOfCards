247812d7976239c212ce5d0d10bd4bb8559e5798
Roman Isecke <rbisecke@gmail.com>
2017-08-31 21:44:29 -0400

    To show the simplicity, create a new card game based on uno cards

---
+++ src/main/java/com/romanisecke/UnoStyle/UnoDeck.java
@@ -0,0 +1,62 @@
+package com.romanisecke.UnoStyle;
+
+import com.romanisecke.AbstractDeck;
+import com.romanisecke.PlayingCard;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class UnoDeck extends AbstractDeck {
+    private LinkedList<PlayingCard> unoPlayingCards = new LinkedList<PlayingCard>();
+
+    public LinkedList<PlayingCard> getUnoPlayingCards() {
+        if (unoPlayingCards == null) {
+            return new LinkedList<PlayingCard>();
+        }
+        return unoPlayingCards;
+    }
+
+    public void setUnoPlayingCards(LinkedList<PlayingCard> unoPlayingCards) {
+        this.unoPlayingCards = unoPlayingCards;
+    }
+
+    @Override
+    public List<PlayingCard> getPlayingCards() {
+        return getUnoPlayingCards();
+    }
+
+    @Override
+    public void addCard(PlayingCard playingCard) {
+        unoPlayingCards.add(playingCard);
+    }
+
+    public UnoDeck() {
+        initializeDeck();
+    }
+
+    @Override
+    public void initializeDeck() {
+        for (String suit : UnoPlayingCard.possibleSuits) {
+            if (suit == null) {
+                continue;
+            }
+            for (String value : UnoPlayingCard.possibleValue) {
+                if ("wild_draw4".equals(value) || "wild".equals(value)) {
+                    continue;
+                }
+                if ("0".equals(value)) {
+                    addCard(new UnoPlayingCard(suit, value));
+                } else {
+                    // Everything else there's 2 of
+                    addCard(new UnoPlayingCard(suit, value));
+                    addCard(new UnoPlayingCard(suit, value));
+                }
+            }
+            for (int i = 0; i < 4; i++) {
+                addCard(new UnoPlayingCard(null, "wild_draw4"));
+                addCard(new UnoPlayingCard(null, "wild"));
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/romanisecke/UnoStyle/UnoPlayingCard.java
@@ -0,0 +1,55 @@
+package com.romanisecke.UnoStyle;
+
+import com.romanisecke.PlayingCard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UnoPlayingCard implements PlayingCard {
+
+    public static final List<String> possibleSuits =
+            new ArrayList(Arrays.asList("red", "green", "yellow", "blue", null));
+    public static final List<String> possibleValue =
+            new ArrayList(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "draw 2", "skip", "reverse", "wild_draw4", "wild"));
+
+    private String suit;
+
+    private String value;
+    @Override
+    public String getFirstValue() {
+        return suit;
+    }
+
+    @Override
+    public String getSecondValue() {
+        return value;
+    }
+
+    @Override
+    public boolean isEqual(Object obj) {
+        return obj instanceof UnoPlayingCard
+                && ((UnoPlayingCard)obj).getFirstValue().equals(getFirstValue())
+                && ((UnoPlayingCard)obj).getSecondValue().equals(getSecondValue());
+    }
+
+    @Override
+    public boolean isFirstValueValid(String suit) {
+        return possibleSuits.contains(suit);
+    }
+
+    @Override
+    public boolean isSecondValueValid(String value) {
+        return possibleValue.contains(value);
+    }
+
+    public UnoPlayingCard(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return getFirstValue() == null ? getSecondValue() : String.format("%s %s", getFirstValue(), getSecondValue());
+    }
+}
